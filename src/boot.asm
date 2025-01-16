e98098f6a99508dbf6dc958dde0547b2ea12fa29
Joseph Perthold <Bedlesssgod@gmail.com>
2025-01-16 12:32:14 +0100

    Added String Buffer

--- src/boot.asm
+++ src/boot.asm
@@ -1,57 +1,73 @@
 [org 0x7c00]
 [bits 16]
 
-mov bx, string
+jmp start
 
-jmp readInput
-
-set_write:
-	mov ah, 0x0e
-	ret
-
-set_read:
-	mov ah, 0x00
-	ret
-
-execute_write:
-	mov ah, 0x0e
-	int 0x10
-	ret
-
-execute_read:
-	mov ah, 0x00
-	int 0x16
-	ret
-
-readInput:
-	call execute_read
-	call execute_write
-	cmp al, '1'
-	je print_exit_term
-	jmp readInput
-
-printString:
-	mov al, [bx] ; Move the current character into al
-	cmp al, 0 ; Check if it is the null terminator (End of string)
-	je terminate ; If it is, return
-	call execute_write
-	inc bx ; Move to the next character
-	jmp printString ; Repeat for the next character
-
-print_exit:
-	mov bx, string
-	call printString
-	ret
-
-print_exit_term:
-	call print_exit
-	jmp terminate
-
-terminate:
-	jmp $
+buffer:
+    times 12 db 0
 
 string:
-	db 'Exited', 0
+    db 'Exited', 0
+
+start:
+    mov bx, buffer
+    mov cx, 12
+
+readInput:
+    call execute_read
+    cmp al, 13
+    je print_exit_term
+    call execute_write
+    mov [bx], al
+    inc bx
+    loop readInput
+
+print_exit_term:
+    call print_exit
+    call print_buffer
+    jmp terminate
+
+print_exit:
+    mov bx, string
+
+printString:
+    mov al, [bx]
+    cmp al, 0
+    je done_print
+    call execute_write
+    inc bx
+    jmp printString
+
+done_print:
+    ret
+
+print_buffer:
+    mov bx, buffer
+    mov cx, 12
+
+print_buffer_loop:
+    mov al, [bx]
+    cmp al, 0
+    je done_buffer
+    call execute_write
+    inc bx
+    loop print_buffer_loop
+
+done_buffer:
+    ret
+
+execute_write:
+    mov ah, 0x0e
+    int 0x10
+    ret
+
+execute_read:
+    mov ah, 0x00
+    int 0x16
+    ret
+
+terminate:
+    jmp $
 
 times 510-($-$$) db 0
-db 0x55, 0xaa
+dw 0xAA55
